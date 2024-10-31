1f9100d73aab9804976104ebcb64f63c228554f6
junioorbernales <wilianjunior.bernales01@estudiant.upf.edu>
2024-10-31 17:38:35 +0100

    Ffmpeg e imagen a usar importadas

--- P1/first_seminar.py
+++ P1/first_seminar.py
@@ -1,11 +1,33 @@
+import ffmpeg
+
 class RGB:
+
+    y = 0
+    u = 0
+    v = 0
+
     def __init__(self, r, g, b):
         self.r = r
         self.g = g
         self.b = b
-    
+
     def rgb_to_yuv(self):
-        Y = (0,257 * self.r) + (0,504 * self.g) + (0,098 * self.b) + 16
-        U = (-0,148 * self.r) - (0,291 * self.g) + (0,439 * self.b) + 128
-        V = (0,439 * self.r) - (0,368 * self.g) - (0,071 * self.b) + 128 
-        return Y, U, V
+        self.y = (0.257 * self.r) + (0.504 * self.g) + (0.098 * self.b) + 16
+        self.u = (-0.148 * self.r) - (0.291 * self.g) + (0.439 * self.b) + 128
+        self.v = (0.439 * self.r) - (0.368 * self.g) - (0.071 * self.b) + 128 
+    
+    def print_RGB_values(self):
+        print("R:", self.r, "\n")
+        print("G:", self.g, "\n")
+        print("B:", self.b, "\n")
+
+    def print_YUV_values(self):
+        print("Y:", self.y, "\n")
+        print("U:", self.u, "\n")
+        print("V:", self.v, "\n")
+    
+i1 = RGB(100, 50, 255)
+i1.print_RGB_values()
+i1.rgb_to_yuv()
+i1.print_YUV_values()
+
