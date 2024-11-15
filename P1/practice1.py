2156b559f46857e8949d30bcfd14f0e2f1312173
junioorbernales <wilianjunior.bernales01@estudiant.upf.edu>
2024-11-15 15:14:26 +0100

    El servidor funciona

--- P1/practice1.py
+++ P1/practice1.py
@@ -1,10 +1,13 @@
 '''
 %pip3 install fastapi
 %pip3 install "uvicorn[standard]" #Servidor virtual para usar FastAPI
+uvicorn P1.practice1:app --reload para encender el servidor
 '''
 
 from fastapi import FastAPI
+
 app = FastAPI()
+
 @app.get("/")
 async def root():
- return {"greeting":"Hello world"}
+    return {"greeting":"Hello world"}
